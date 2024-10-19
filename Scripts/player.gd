3810494afd2ed716d8a859e9152616013d40dd2a
AstonisherVR <152096497+AstonisherVR@users.noreply.github.com>
2024-10-19 23:47:35 +0300

    Finishing the items. Only left to ACTUALLY IMPLEMENT THE FUNCTIONALITY.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -26,36 +26,41 @@ func handle_items():
 		selected_item = Items.HAMMER
 	if Input.is_action_just_pressed("Item_Three"):
 		selected_item = Items.MIRROR
+	if Input.is_action_just_pressed("Item_Four"):
+		selected_item = Items.DOG_WHISTLE
 
 	if selected_item == Items.FLASHLIGHT:
-		set_visible_item(1)
-		if Input.is_action_pressed("R_Click"): 
+		set_visible_item(selected_item)
+		if Input.is_action_pressed("R_Click") or Input.is_action_pressed("L_Click"): 
 			flashlight.show()
 			flashlight.position = get_global_mouse_position()
 		else:
 			flashlight.hide()
 	elif selected_item == Items.HAMMER:
-		set_visible_item(2)
+		set_visible_item(selected_item)
 		hammer.position = get_global_mouse_position()
-		if Input.is_action_just_pressed("R_Click"): 
+		if Input.is_action_just_pressed("R_Click") or Input.is_action_pressed("L_Click"): 
 			items_animation.play("Hammer Boink")
 			#hammer_sfx.play()
 	elif selected_item == Items.MIRROR:
-		set_visible_item(3)
+		set_visible_item(selected_item)
 		mirror.position = get_global_mouse_position()
+	elif selected_item == Items.DOG_WHISTLE:
+		set_visible_item(selected_item)
+		dog_whistle.position = get_global_mouse_position()
 
 func set_visible_item(number):
-	if number == 1:
+	if number == 0:
 		flashlight.show()
 		hammer.hide()
 		mirror.hide()
 		dog_whistle.hide()
-	elif number == 2:
+	elif number == 1:
 		flashlight.hide()
 		hammer.show()
 		mirror.hide()
 		dog_whistle.hide()
-	elif number == 3:
+	elif number == 2:
 		flashlight.hide()
 		hammer.hide()
 		mirror.show()
@@ -63,5 +68,5 @@ func set_visible_item(number):
 	elif number == 3:
 		flashlight.hide()
 		hammer.hide()
-		mirror.show()
+		mirror.hide()
 		dog_whistle.show()
