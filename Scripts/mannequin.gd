0080508848e9d4defcabf80580757ad4fb123f31
AstonisherVR <152096497+AstonisherVR@users.noreply.github.com>
2024-10-21 22:40:52 +0300

    Almost finished Defending mechanic

--- Scripts/mannequin.gd
+++ Scripts/mannequin.gd
@@ -1,207 +1,206 @@
 extends CharacterBody2D
 
-# I like to export the variables 
-## so that when I press a dot next to the variable "movement_timer." 
-### GoDot will show me a list of all the things that I can do.
-enum Stages {STAGE_0, STAGE_1, STAGE_2, STAGE_3, STAGE_4, STAGE_5_KILL}	 # These are the stages of the enemy.
-enum Masks {NO_MASK, NEUTRAL_MASK, HAPPY_MASK, SAD_MASK, WOLF_MASK}	# These are the masks that it can wear.
+# I like to export the variables so that GoDot will show me
+# a list of all the things that I can do after pressing a dot.
+enum Stages {STAGE_0, STAGE_1, STAGE_2, STAGE_3, STAGE_4, STAGE_5_KILL} 	# These are the stages of the enemy.
+enum Masks {NO_MASK, NEUTRAL_MASK, HAPPY_MASK, SAD_MASK, WOLF_MASK}			# These are the masks that it can wear.
+enum States {ATTACKING, STUNNED, RECOVERING}
 
+@export_group("Sprite References")
 @export var mannequin_stand_sprite: Sprite2D
-@export var mannequin_forawrd_sprite: Sprite2D
+@export var mannequin_forward_sprite: Sprite2D
 
+@export_group("Small Masks")
 @export var small_neutral_mask: Sprite2D
 @export var small_happy_mask: Sprite2D
 @export var small_sad_clown_mask: Sprite2D
 @export var small_wolf_mask: Sprite2D
 
+@export_group("Big Masks")
 @export var big_neutral_mask: Sprite2D
 @export var big_happy_mask: Sprite2D
 @export var big_sad_clown_mask: Sprite2D
 @export var big_wolf_mask: Sprite2D
 
-@export var movement_timer: Timer	# This makes the enemy move. 
+@export_group("Timers")
+@export var movement_timer: Timer	# This makes the enemy move
 @export var kill_countdown: Timer	# Countdown until the enemy will kill.
+@export var stunned_timer: Timer	# Stunned for a period of time.
+@export var recovery_timer: Timer	# This makes the enemy recover after it's been defeated.
 
-@export var marker_points: Array[Marker2D]	# Those are the positions of where the enemy can go.
-
+@export_group("Gameplay Properties")
+@export var collisions: Array[CollisionShape2D]
+@export var marker_points: Array[Marker2D]
+@export var health_value: int		# This is the value assigned to the main health.
 @export var agression_level: int	# This is how agressive the enemy is. Will change durning the night.
 @export var attack_frequency: int	# This is the time for how often the enemy has a chance to move. Changing it makes the enemy move more often.
-@export var chance_to_mask: int	 # This is how much the enemy has a chance to put on a mask. 
+@export var stunned_time: int		# This is how long it takes to be defeated.
+@export var recovery_time: int		# This is how long it will take the enemy to start.
+@export var chance_to_mask: int		# This is how much the enemy has a chance to put on a mask. 
 
-@export var current_stage := Stages.STAGE_0
-@export var current_mask := Masks.NO_MASK
+var main_health: int				# When it raches 0, the mannequin goes back to Stage 0.
+var current_stage: Stages = Stages.STAGE_0
+var current_mask: Masks = Masks.NO_MASK
+var current_state: States = States.ATTACKING
+var taking_damage: bool = false
 
 func _ready() -> void:
+	initialize_enemy()
+
+func _process(delta: float) -> void:
+	if taking_damage:
+		apply_damage(1 * delta)
+
+func initialize_enemy() -> void:
+	main_health = health_value
 	set_z_ordering(-2)
 	set_sprite(0)
+	movement_timer.wait_time = attack_frequency
 	movement_timer.start()
 
-func _physics_process(delta: float) -> void:
-	pass
+func apply_damage(damage: int) -> void:
+	main_health -= damage
+	if main_health <= 0:
+		reset_enemy()
 
-func _on_movement_timer_timeout() -> void: # When the timer is done, the enemy has a chance to move.
+func update_ai() -> void:
+	if agression_level >= randi() % 100 + 1:
+		match current_stage:
+			Stages.STAGE_0, Stages.STAGE_1, Stages.STAGE_2:
+				advance_position(0, -1)
+			Stages.STAGE_3:
+				advance_position(1, 4)
+			Stages.STAGE_4:
+				if kill_countdown.is_stopped():
+					kill_countdown.start()
+			Stages.STAGE_5_KILL:
+				kill_player()
+		
+		if should_change_mask():
+			change_mask()
+
+func advance_position(sprite_index: int, z_order: int) -> void:
+	if current_state == States.ATTACKING:
+		current_stage += 1
+		if current_stage < marker_points.size() and marker_points[current_stage]:
+			position = marker_points[current_stage].position
+			set_sprite(sprite_index)
+			set_z_ordering(z_order)
+
+func should_change_mask() -> bool:
+	return kill_countdown.is_stopped() and \
+		   chance_to_mask >= randi() % 100 + 1 and \
+		   current_stage <= Stages.STAGE_4
+
+func change_mask() -> void:
+	current_mask = Masks.values().pick_random()
+	set_current_mask(current_mask, current_stage)
+	set_active_collisions(current_mask, current_stage)
+
+func set_sprite(number: int) -> void:
+	mannequin_stand_sprite.visible = (number == 0)
+	mannequin_forward_sprite.visible = (number == 1)
+
+func set_z_ordering(number: int) -> void:
+	z_index = number
+
+func set_current_mask(mask: Masks, stage: Stages) -> void:
+	hide_all_masks()
+	if stage < Stages.STAGE_4:
+		show_small_mask(mask)
+	else:
+		show_big_mask(mask)
+
+func hide_all_masks() -> void:
+	var all_masks = [
+		small_neutral_mask, small_happy_mask, small_sad_clown_mask, small_wolf_mask,
+		big_neutral_mask, big_happy_mask, big_sad_clown_mask, big_wolf_mask
+	]
+	for mask in all_masks:
+		mask.hide()
+
+func show_big_mask(mask: Masks) -> void:
+	match mask:
+		Masks.NEUTRAL_MASK: big_neutral_mask.show()
+		Masks.HAPPY_MASK: big_happy_mask.show()
+		Masks.SAD_MASK: big_sad_clown_mask.show()
+		Masks.WOLF_MASK: big_wolf_mask.show()
+		_: pass
+
+func show_small_mask(mask: Masks) -> void:
+	match mask:
+		Masks.NEUTRAL_MASK: small_neutral_mask.show()
+		Masks.HAPPY_MASK: small_happy_mask.show()
+		Masks.SAD_MASK: small_sad_clown_mask.show()
+		Masks.WOLF_MASK: small_wolf_mask.show()
+		_: pass
+
+func set_active_collisions(mask: Masks, stage: Stages) -> void:
+	for collision in collisions:
+		collision.disabled = true
+	
+	var collision_index = -1
+	match mask:
+		Masks.NEUTRAL_MASK: collision_index = 0
+		Masks.HAPPY_MASK: collision_index = 1
+		Masks.SAD_MASK: collision_index = 2
+		_: return
+	
+	if stage == Stages.STAGE_4:
+		collision_index += 3
+	
+	if collision_index >= 0 and collision_index < collisions.size():
+		collisions[collision_index].disabled = false
+
+# Timer Callbacks
+func _on_movement_timer_timeout() -> void:
 	update_ai()
-	movement_timer.wait_time = attack_frequency
 	movement_timer.start()
 
 func _on_kill_countdown_timeout() -> void:
 	current_stage = Stages.STAGE_5_KILL
 
-func update_ai():
-	if agression_level >= randi()%100+1:
-		match current_stage:
-			Stages.STAGE_0:
-				ai_move()
-				set_sprite(0)
-				set_z_ordering(-1)
-			Stages.STAGE_1:
-				ai_move()
-				set_sprite(0)
-				set_z_ordering(-1)
-			Stages.STAGE_2:
-				ai_move()
-				set_sprite(0)
-				set_z_ordering(-1)
-			Stages.STAGE_3:
-				ai_move()
-				set_sprite(1)
-				set_z_ordering(4)
-			Stages.STAGE_4:
-				if kill_countdown.is_stopped(): # This is to make sure the timer gets triggered only once.
-					kill_countdown.start()
-			Stages.STAGE_5_KILL:
-				kill_player()
-		if kill_countdown.is_stopped():
-			if chance_to_mask >= randi()%100+1 and current_stage <= 4:
-				current_mask = Masks.values().pick_random()
-				print("current_stage ", current_stage)
-				print("current_mask ", current_mask)
-				match current_mask:
-					Masks.NO_MASK:
-						set_current_mask(Masks.NO_MASK, current_stage)
-					Masks.NEUTRAL_MASK:
-						set_current_mask(Masks.NEUTRAL_MASK, current_stage)
-					Masks.HAPPY_MASK:
-						set_current_mask(Masks.HAPPY_MASK, current_stage)
-					Masks.SAD_MASK:
-						set_current_mask(Masks.SAD_MASK, current_stage)
-					Masks.WOLF_MASK:
-						set_current_mask(Masks.WOLF_MASK, current_stage)
+func _on_stunned_timer_timeout() -> void:
+	taking_damage = false
+	current_state = States.RECOVERING
+	recovery_timer.start(recovery_time)
 
-func ai_move():
-	current_stage += 1
-	if marker_points[current_stage]:
-		position = marker_points[current_stage].position
+func _on_recovery_timer_timeout() -> void:
+	main_health = health_value
+	current_state = States.ATTACKING
+	current_stage = Stages.STAGE_0
+	movement_timer.start()
 
-func hammer_hit():
-	print("OUCH! THAT HURT!")
+# Player Interaction Methods
+func hammer_hit() -> void:
+	if current_mask in [Masks.NO_MASK, Masks.NEUTRAL_MASK]:
+		stun_enemy()
 
-func reflection_shown():
-	print("AHH! I'M HEDOUS!")
+func flashlight_shined() -> void:
+	if current_mask == Masks.HAPPY_MASK:
+		stun_enemy()
 
-func flashed():
-	print("I CAN'T SEE!")
+func reflection_shown() -> void:
+	if current_mask == Masks.SAD_MASK:
+		stun_enemy()
 
-func whistle_used():
-	print("MY EARS!")
+func whistle_used() -> void:
+	if current_mask == Masks.WOLF_MASK:
+		stun_enemy()
 
-func no_movement_detected():
-	print("Good kid.") 
+func stun_enemy() -> void:
+	taking_damage = true
+	current_state = States.STUNNED
+	movement_timer.stop()
+	stunned_timer.start(stunned_time)
 
-func kill_player():
+func reset_enemy() -> void:
+	current_stage = Stages.STAGE_0
+	current_state = States.RECOVERING
+	taking_damage = false
+	position = marker_points[0].position if marker_points.size() > 0 else Vector2.ZERO
+	recovery_timer.start(recovery_time)
+
+func kill_player() -> void:
 	print_rich("[color=red][b]Game Over![/b][/color]")
-	#get_tree().paused = true
-
-func set_sprite(number):
-	if number == 0:
-		mannequin_stand_sprite.show()
-		mannequin_forawrd_sprite.hide()
-	elif number == 1:
-		mannequin_stand_sprite.hide()
-		mannequin_forawrd_sprite.show()
-
-func set_z_ordering(number):
-	z_index = number
-
-func set_current_mask(current_mask_number: int, current_stage_number: int):
-	if current_mask_number == 0:
-		small_neutral_mask.hide()
-		small_happy_mask.hide()
-		small_sad_clown_mask.hide()
-		small_wolf_mask.hide()
-		big_neutral_mask.hide()
-		big_happy_mask.hide()
-		big_sad_clown_mask.hide()
-		big_wolf_mask.hide()
-	elif current_mask_number == 1 and current_stage_number < 4:
-		small_neutral_mask.show()
-		small_happy_mask.hide()
-		small_sad_clown_mask.hide()
-		small_wolf_mask.hide()
-		big_neutral_mask.hide()
-		big_happy_mask.hide()
-		big_sad_clown_mask.hide()
-		big_wolf_mask.hide()
-	elif current_mask_number == 1 and current_stage_number >= 4: 
-		small_neutral_mask.hide()
-		small_happy_mask.hide()
-		small_sad_clown_mask.hide()
-		small_wolf_mask.hide()
-		big_neutral_mask.show()
-		big_happy_mask.hide()
-		big_sad_clown_mask.hide()
-		big_wolf_mask.hide()
-	elif current_mask_number == 2 and current_stage_number < 4:
-		small_neutral_mask.hide()
-		small_happy_mask.show()
-		small_sad_clown_mask.hide()
-		small_wolf_mask.hide()
-		big_neutral_mask.hide()
-		big_happy_mask.hide()
-		big_sad_clown_mask.hide()
-		big_wolf_mask.hide()
-	elif current_mask_number == 2 and current_stage_number >= 4:
-		small_neutral_mask.hide()
-		small_happy_mask.hide()
-		small_sad_clown_mask.hide()
-		small_wolf_mask.hide()
-		big_neutral_mask.hide()
-		big_happy_mask.show()
-		big_sad_clown_mask.hide()
-		big_wolf_mask.hide()
-	elif current_mask_number == 3 and current_stage_number < 4:
-		small_neutral_mask.hide()
-		small_happy_mask.hide()
-		small_sad_clown_mask.show()
-		small_wolf_mask.hide()
-		big_neutral_mask.hide()
-		big_happy_mask.hide()
-		big_sad_clown_mask.hide()
-		big_wolf_mask.hide()
-	elif current_mask_number == 3 and current_stage_number >= 4:
-		small_neutral_mask.hide()
-		small_happy_mask.hide()
-		small_sad_clown_mask.hide()
-		small_wolf_mask.hide()
-		big_neutral_mask.hide()
-		big_happy_mask.hide()
-		big_sad_clown_mask.show()
-		big_wolf_mask.hide()
-	elif current_mask_number == 4 and current_stage_number < 4:
-		small_neutral_mask.hide()
-		small_happy_mask.hide()
-		small_sad_clown_mask.hide()
-		small_wolf_mask.show()
-		big_neutral_mask.hide()
-		big_happy_mask.hide()
-		big_sad_clown_mask.hide()
-		big_wolf_mask.hide()
-	elif current_mask_number == 4 and current_stage_number >= 4:
-		small_neutral_mask.hide()
-		small_happy_mask.hide()
-		small_sad_clown_mask.hide()
-		small_wolf_mask.hide()
-		big_neutral_mask.hide()
-		big_happy_mask.hide()
-		big_sad_clown_mask.hide()
-		big_wolf_mask.show()
+	# Implement actual game over logic here
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,86 +1,152 @@
 extends CharacterBody2D
 
-enum Items {FLASHLIGHT, HAMMER, MIRROR, DOG_WHISTLE}
-var selected_item := Items.FLASHLIGHT
+#TODO ADD A GODDAM GAMEPLAY LOOP
 
-@export var mannequin_enemy: Node2D
-@export var camera: Camera2D
+signal item_used(item_name: String)
+signal item_switched(item: Items)
+
+enum Items {FLASHLIGHT, HAMMER, MIRROR, DOG_WHISTLE}
+
+@export_group("Item References")
+@export var item_collisions: Array[CollisionShape2D]
 @export var flashlight: Node2D
 @export var hammer: Node2D
 @export var mirror: Node2D
 @export var dog_whistle: Node2D
+
+@export_group("Game Components")
+@export var mannequin_enemy: Node2D
+@export var camera: Camera2D
 @export var items_animation: AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
+@export_group("Audio")
+@export var dog_whistle_sfx: AudioStreamPlayer2D
+
+@export_group("Interaction Areas")
+@export var flashlight_area: Area2D
+@export var hammer_area: Area2D
+@export var mirror_area: Area2D
+
+# State variables
+var selected_item := Items.FLASHLIGHT
+var last_mouse_position := Vector2.ZERO
+var mouse_still_time: float
+
 func _ready() -> void:
-	pass
+	set_visible_item(selected_item)
+	set_active_collisions()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	update_camera()
+	handle_item_selection()
+	handle_item_behavior()
+	handle_mouse_stillness(delta)
+
+func update_camera() -> void:
 	camera.position = -get_global_mouse_position()
-	handle_items()
 
-func handle_items():
+func handle_item_selection() -> void:
 	if Input.is_action_just_pressed("Item_One"):
-		selected_item = Items.FLASHLIGHT
-	if Input.is_action_just_pressed("Item_Two"):
-		selected_item = Items.HAMMER
-	if Input.is_action_just_pressed("Item_Three"):
-		selected_item = Items.MIRROR
-	if Input.is_action_just_pressed("Item_Four"):
-		selected_item = Items.DOG_WHISTLE
+		switch_to_item(Items.FLASHLIGHT)
+	elif Input.is_action_just_pressed("Item_Two"):
+		switch_to_item(Items.HAMMER)
+	elif Input.is_action_just_pressed("Item_Three"):
+		switch_to_item(Items.MIRROR)
+	elif Input.is_action_just_pressed("Item_Four"):
+		switch_to_item(Items.DOG_WHISTLE)
 
-	if selected_item == Items.FLASHLIGHT:
-		set_visible_item(selected_item)
-		if Input.is_action_pressed("R_Click") or Input.is_action_pressed("L_Click"): 
-			flashlight.show()
-			flashlight.position = get_global_mouse_position()
-		else:
-			flashlight.hide()
-	elif selected_item == Items.HAMMER:
-		set_visible_item(selected_item)
-		hammer.position = get_global_mouse_position()
-		if Input.is_action_just_pressed("R_Click") or Input.is_action_pressed("L_Click"): 
-			items_animation.play("Hammer Boink")
-			#hammer_sfx.play()
-	elif selected_item == Items.MIRROR:
-		set_visible_item(selected_item)
-		mirror.position = get_global_mouse_position()
-	elif selected_item == Items.DOG_WHISTLE:
-		set_visible_item(selected_item)
-		dog_whistle.position = get_global_mouse_position()
+func switch_to_item(new_item: Items) -> void:
+	selected_item = new_item
+	set_visible_item(selected_item)
+	set_active_collisions()
+	dog_whistle_sfx.playing = false
 
-func set_visible_item(number):
-	if number == 0:
-		flashlight.show()
-		hammer.hide()
-		mirror.hide()
-		dog_whistle.hide()
-	elif number == 1:
-		flashlight.hide()
-		hammer.show()
-		mirror.hide()
-		dog_whistle.hide()
-	elif number == 2:
-		flashlight.hide()
-		hammer.hide()
-		mirror.show()
-		dog_whistle.hide()
-	elif number == 3:
-		flashlight.hide()
-		hammer.hide()
-		mirror.hide()
-		dog_whistle.show()
+func handle_item_behavior() -> void:
+	var mouse_pos = get_global_mouse_position()
+	var is_action = Input.is_action_pressed("R_Click") or Input.is_action_pressed("L_Click")
+	match selected_item:
+		Items.FLASHLIGHT:
+			handle_flashlight(mouse_pos, is_action)
+		Items.HAMMER:
+			handle_hammer(mouse_pos, is_action)
+		Items.MIRROR:
+			handle_mirror(mouse_pos)
+		Items.DOG_WHISTLE:
+			handle_dog_whistle(mouse_pos, is_action)
 
-func interact_with_enemy(enemy):
-	match enemy.current_mask:
-		enemy.Masks.NO_MASK:
-			enemy.no_movement_detected()
-		enemy.Masks.NEUTRAL_MASK:
-			enemy.hammer_hit()
-		enemy.Masks.HAPPY_MASK:
-			enemy.flashed()
-		enemy.Masks.SAD_MASK:
-			enemy.reflection_shown()
-		enemy.Masks.WOLF_MASK:
-			enemy.whistle_used()
+func handle_flashlight(mouse_pos: Vector2, is_action: bool) -> void:
+	flashlight.position = mouse_pos
+	flashlight.visible = is_action
+	if flashlight_area.overlaps_area(mannequin_enemy) and is_action:
+		mannequin_enemy.flashlight_shined()
+
+func handle_hammer(mouse_pos: Vector2, is_action: bool) -> void:
+	hammer.position = mouse_pos
+	if Input.is_action_just_pressed("R_Click") or Input.is_action_just_pressed("L_Click"):
+		items_animation.play("Hammer Boink")
+		if hammer_area.overlaps_area(mannequin_enemy):
+			mannequin_enemy.hammer_hit()
+
+func handle_mirror(mouse_pos: Vector2) -> void:
+	mirror.position = mouse_pos
+	if mirror_area.overlaps_area(mannequin_enemy):
+		mannequin_enemy.reflection_shown()
+
+func handle_dog_whistle(mouse_pos: Vector2, is_action: bool) -> void:
+	dog_whistle.position = mouse_pos
+	dog_whistle_sfx.playing = is_action
+	
+	if is_action and mannequin_enemy.current_mask == mannequin_enemy.Masks.WOLF_MASK:
+		mannequin_enemy.apply_damage(2)
+
+func handle_mouse_stillness(delta: float) -> void:
+	var current_mouse_pos = get_global_mouse_position()
+	if current_mouse_pos == last_mouse_position:
+		mouse_still_time += delta
+		if mouse_still_time >= 1.0 and mannequin_enemy.current_mask == mannequin_enemy.Masks.NO_MASK:
+			mannequin_enemy.apply_damage(1)
+	else:
+		mouse_still_time = 0.0
+	last_mouse_position = current_mouse_pos
+
+func set_visible_item(item: Items) -> void:
+	flashlight.visible = item == Items.FLASHLIGHT
+	hammer.visible = item == Items.HAMMER
+	mirror.visible = item == Items.MIRROR
+	dog_whistle.visible = item == Items.DOG_WHISTLE
+	
+func set_active_collisions() -> void:
+	for collision_shape in item_collisions:
+		collision_shape.disabled = true
+	if selected_item < item_collisions.size():
+		item_collisions[selected_item].disabled = false
+#
+#func interact_with_enemy() -> void:
+	#match selected_item:
+		#Items.FLASHLIGHT:
+			#if flashlight_area.overlaps_area(mannequin_enemy) and Input.is_action_pressed("R_Click"):
+				#mannequin_enemy.flashlight_shined()
+		#Items.HAMMER:
+			#if hammer_area.overlaps_area(mannequin_enemy) and Input.is_action_just_pressed("R_Click"):
+				#mannequin_enemy.hammer_hit()
+		#Items.MIRROR:
+			#if mirror_area.overlaps_area(mannequin_enemy):
+				#mannequin_enemy.reflection_shown()
+		#Items.DOG_WHISTLE:
+			#if mannequin_enemy.current_mask == mannequin_enemy.Masks.WOLF_MASK:
+				#mannequin_enemy.apply_damage(2)
+#
+#func _on_flashlight_area_2d_area_entered(area: Area2D) -> void:
+	#if area.is_in_group("Happy Masks"):
+		#mannequin_enemy.flashlight_shined() # Apply damage
+		#print(mannequin_enemy.current_state)
+#
+#func _on_hammer_area_2d_area_entered(area: Area2D) -> void:
+	#if area.is_in_group("Neutral Masks"):
+		#mannequin_enemy.hammer_hit() # Apply damage
+		#print(mannequin_enemy.current_state)
+#
+#func _on_mirror_area_2d_area_entered(area: Area2D) -> void:
+	#if area.is_in_group("Sad Masks"):
+		#mannequin_enemy.reflection_shown() # Apply damage
+		#print(mannequin_enemy.current_state)
